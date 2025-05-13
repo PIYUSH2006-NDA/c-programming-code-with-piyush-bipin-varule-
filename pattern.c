7bbca5b1836d9596b2e210d9cb273df979cd7495
PIYUSH BIPIN VARULE <piyushvarule15@gmail.com>
2025-05-14 01:07:54 +0530

    Create pattern.c
    12345
    2345
    345
    45
    5

---
+++ pattern.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = i; j <= 5; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
